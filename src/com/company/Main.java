a91bf7d16191aa0af75edbb1979685444fbd180b
HerpPotato <10009702@sbstudents.org>
2016-05-03 18:53:09 -0400

    Small additions to basics of the outline

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,88 @@
 package com.company;
 
-public class Main
+import java.awt.FontFormatException;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.HashMap;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Sequencer;
+
+public class Main extends Application
 {
-    public static void main(String[] args)
+    final int tileSize = 16;
+    final int width = 80;
+    final int height = 45;
+    final int pixelWidth = tileSize * width;
+    final int pixelHeight = tileSize * height;
+
+    @Override
+    public void start(Stage primaryStage)
+    {
+        primaryStage.setTitle("Test");
+        Canvas canvas = new Canvas(pixelWidth, pixelHeight);
+        StackPane root = new StackPane();
+        root.getChildren().add(canvas);
+        Scene scene = new Scene(root, pixelWidth, pixelHeight);
+        scene.setOnKeyPressed(this::onKeyPressed);
+        scene.setOnKeyReleased(this::onKeyReleased);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        KeyFrame frame = new KeyFrame(Duration.millis(4f),
+            new EventHandler<ActionEvent>()
+            {
+                @Override
+                public void handle(ActionEvent event)
+                {
+                    canvas.getGraphicsContext2D().clearRect(0, 0, pixelWidth, pixelHeight);
+                }
+            });
+    }
+
+    public void keyFrame()
     {
 
+    }
+
+    public void onKeyPressed(KeyEvent e)
+    {
+
+    }
+
+    public void onKeyReleased(KeyEvent e)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 }
